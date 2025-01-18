eb2f8548b085d5b178ae37e5b58ee7938110080a
weirdbeardgame <finalfantasytimothy@gmail.com>
2025-01-18 04:53:03 -0700

    Object.c

--- src/graphics/graph3d/object.c
+++ src/graphics/graph3d/object.c
@@ -1,6 +1,28 @@
 #include "common.h"
+#include "typedefs.h"
+#include "object.h"
 
-INCLUDE_ASM(const s32, "graphics/graph3d/object", objInit);
+#include "graphics/graph3d/sglib.h"
 
-void objFlush(void) {
+qword *pstart1 = NULL;
+qword *pstart3 = NULL;
+
+void objInit()
+{
+	static int bufid = 0;
+	static qword p3_buf[2][256];
+	qword *base3;
+
+    pstart3 = p3_buf[bufid];
+    bufid = 1 - bufid;
+    pbase3 = pstart3;
+    SgSetWorkBase(pbase3);
+}
+
+qword *pbase1 = NULL;
+qword *pbase3 = NULL;
+
+ void objFlush()
+{
+    return;
 }
