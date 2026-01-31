ac4d04754051952d2a8d5ac863bc29734dc4d623
Claudio Baecchi <claudio.baecchi@gmail.com>
2026-01-31 12:01:51 +0100

    decompiled ingame/map/furn_spe/fspe_acs.c
    Co-authored-by: Frozen Burnside <fmilesi_soft@hotmail.com>

--- src/ingame/map/furn_spe/fspe_acs.c
+++ src/ingame/map/furn_spe/fspe_acs.c
@@ -1,5 +1,44 @@
 #include "common.h"
+#include "typedefs.h"
+#include "furn_spe.h"
 
-INCLUDE_ASM(const s32, "ingame/map/furn_spe/fspe_acs", FSpeMapDataMapping);
+ #define BASE_ADDRESS 0x7f8000
 
-INCLUDE_ASM(const s32, "ingame/map/furn_spe/fspe_acs", FSpeGetTopAddr);
+void FSpeMapDataMapping()
+{
+    u_int *addr_top;
+    u_int *addr_data;
+    int i;
+    u_short data_num;
+
+    addr_top = (u_int *)(BASE_ADDRESS + 4 * 4);
+    addr_top = (u_int *)(*addr_top + BASE_ADDRESS);
+
+    addr_data = (u_int *)(*addr_top + BASE_ADDRESS);
+
+    data_num = ((u_int)addr_data - (u_int)addr_top) / 4;
+
+    addr_data = addr_top;
+
+    for (i = 0; i < data_num; i++)
+    {
+        *addr_data += BASE_ADDRESS;
+
+        addr_data++;
+    }
+}
+
+u_char* FSpeGetTopAddr(u_short fact_no)
+{
+    u_int *addr;
+
+    if (fact_no == 0xffff)
+    {
+        return 0;
+    }
+
+    addr = (u_int *)(BASE_ADDRESS + 4 * 4);
+    addr = (u_int *)(*addr + BASE_ADDRESS);
+
+    return (u_char *)addr[fact_no];
+}
