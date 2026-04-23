ced0e00a5631bad5954055dac19350c75a0b6c42
Claudio Baecchi <claudio.baecchi@gmail.com>
2026-04-23 18:36:38 +0200

    decompiled (jp) and revisited (us,eu) os/system.c

--- src/os/system.c
+++ src/os/system.c
@@ -32,33 +32,50 @@ static int down_fr;
 static void InitIop();
 static void LoadDefModule();
 
+#if defined(BUILD_JP_VERSION)
+#define IOPRP_IMAGE_FILE "cdrom0:\\IOPRP224.IMG;1"
+#elif defined(BUILD_US_VERSION)
+#define IOPRP_IMAGE_FILE "cdrom0:\\IOPRP224.IMG;1"
+#elif defined(BUILD_EU_VERSION)
+#define IOPRP_IMAGE_FILE "cdrom0:\\IOPRP23.IMG;1"
+#endif
+
 void InitSystem()
 {
     InitIop();
     LoadDefModule();
     InitIop();
+
     sceSifInitIopHeap();
     sceFsReset();
+
     LoadIRX();
     InitSysWrk();
+#if defined(BUILD_JP_VERSION)
+    InitOptionWrk();
+#endif
     SetDefaultKeyType();
     InitGraphics();
     InitPad();
     EiInit();
     InitSe();
+#if defined(BUILD_US_VERSION) || defined(BUILD_EU_VERSION)
     InitOptionWrk();
+#endif
+
     sceMcInit();
 
-#ifdef BUILD_EU_VERSION
+#if defined(BUILD_EU_VERSION)
     mc_language = 0;
 #endif
     mc_start_flg = 1;
+
     sceSdRemoteInit();
 }
 
 void EndSystem()
 {
-    //
+    return;
 }
 
 static void InitIop()
@@ -70,11 +87,7 @@ static void InitIop()
 
 static void LoadDefModule()
 {
-#ifdef BUILD_EU_VERSION
-    while (!sceSifRebootIop("cdrom0:\\IOPRP23.IMG;1")) {};
-#else
-    while (!sceSifRebootIop("cdrom0:\\IOPRP224.IMG;1")) {};
-#endif
+    while (!sceSifRebootIop(IOPRP_IMAGE_FILE)) {};
     while (!sceSifSyncIop()) {};
 }
 
@@ -92,6 +105,7 @@ void LoadIRX()
 void InitSysWrk()
 {
     GetNowClock(&sys_wrk.rtc);
+
     sys_wrk.reset = 0;
     sys_wrk.pause = 0;
     sys_wrk.game_mode = GAME_MODE_INIT;
@@ -101,10 +115,11 @@ void InitSysWrk()
 
 void GetNowClock(sceCdCLOCK *nc)
 {
-    memset(nc, 0, sizeof(sceCdCLOCK));
-    
-    if (!sceCdReadClock(nc)) {
-        memset(nc, 0, sizeof(sceCdCLOCK));
+    *nc = (sceCdCLOCK){0};
+
+    if (!sceCdReadClock(nc))
+    {
+        *nc = (sceCdCLOCK){0};
         nc->pad = 1;
     }
 }
@@ -112,46 +127,53 @@ void GetNowClock(sceCdCLOCK *nc)
 void InitOptionWrk()
 {
     opt_wrk.pad_mode = 0;
-    opt_wrk.se_vol = 0x3fff;
-    opt_wrk.bgm_vol = 0xccc;
     opt_wrk.pad_move = 0;
     opt_wrk.key_type = 0;
     opt_wrk.sound_mode = 0;
+
+#if defined(BUILD_JP_VERSION)
+    opt_wrk.bgm_vol = 0x1000;
+    opt_wrk.se_vol = 0x3fff;
+#elif defined(BUILD_US_VERSION) || defined(BUILD_EU_VERSION)
+    opt_wrk.se_vol = 0x3fff;
+    opt_wrk.bgm_vol = 0xccc;
+
     SeSetMVol(0xccc);
     SeSetSteMono(opt_wrk.sound_mode);
+#endif
 }
 
 void InitGraphics()
 {
     sceDmaEnv env;
-    
+
     sceDmaReset(1);
     sceGsResetPath();
     sceDevVif0Reset();
     sceDevVu0Reset();
 
-#ifdef BUILD_EU_VERSION
-    sceGsResetGraph(0, 1, sys_wrk.pal_disp_mode == 0 ? 3 : 2, 1);
-#else
+#if defined(BUILD_JP_VERSION) || defined(BUILD_US_VERSION)
     sceGsResetGraph(0, 1, 2, 1);
+#elif defined(BUILD_EU_VERSION)
+    sceGsResetGraph(0, 1, sys_wrk.pal_disp_mode == 0 ? 3 : 2, 1);
 #endif
-    
+
     *REG_RCNT0_MODE = 0x83;
     *REG_RCNT1_MODE = 0x83;
-    
+
     sceDmaGetEnv(&env);
-    
+
     env.notify = 4;
-    
+
     sceDmaPutEnv(&env);
     sceGsSetDefDBuff(&g_db, 0, 0x280, 0xe0, 2, 0x31, 1);
-    
+
     pdrawenv = &g_db.draw0;
-    
+
     clearGsMem(0, 0, 0, 0x280, 0x1c0);
     SetSysBackColor(0, 0, 0);
 
-#ifdef BUILD_EU_VERSION
+#if defined(BUILD_EU_VERSION)
     if (sys_wrk.pal_disp_mode == 0)
     {
         g_db.disp[1].display.DX = 0x290;
@@ -169,13 +191,12 @@ void InitGraphics()
 #endif
 }
 
-
 int userGsSwapDBuff(sceGsDBuff *db, int id)
 {
     int ret;
-    
+
     ret = 0;
-    
+
     if (id % 2)
     {
         sceGsPutDispEnv(&db->disp[1]);
@@ -184,8 +205,7 @@ int userGsSwapDBuff(sceGsDBuff *db, int id)
     {
         sceGsPutDispEnv(&db->disp[0]);
     }
-    
-    
+
     if (stop_put_draw_env == 0)
     {
         if (id % 2)
@@ -197,14 +217,16 @@ int userGsSwapDBuff(sceGsDBuff *db, int id)
             ret = sceGsPutDrawEnv(&db->giftag0);
         }
     }
-    
+
     return ret;
 }
 
 int vblankHandler(int val)
 {
     down_fr++;
+
     EIntr();
+
     return 0;
 }
 
@@ -212,52 +234,60 @@ void vfunc()
 {
     static int count = 0;
     static int dfro = 0;
-    
+
     StopPerformanceCounter();
-    
+
     if (count < *REG_RCNT1_COUNT)
     {
         count = *REG_RCNT1_COUNT;
     }
-    
+
     if (count > 0xdc || (fr & 0xff) == 0)
     {
         count = 0;
     }
-    
+
     if (down_fr != 0 && down_fr == dfro && odev != (sys_wrk.count & 1))
     {
         sys_wrk.count++;
+
         fr++;
+
         down_fr = 0;
     }
-    
+
     dfro = down_fr;
+
     *REG_RCNT0_COUNT = 0;
     *REG_RCNT1_COUNT = 0;
-    
+
     if (sys_wrk.count & 1)
     {
         pdrawenv = &g_db.draw1;
-        sceGsSetHalfOffset(&g_db.draw0, 0x800, 0x800, (odev & 0xffffU) + 1 & 1);
+
+        sceGsSetHalfOffset(&g_db.draw0, 0x800, 0x800, (odev + 1) & 1);
     }
     else
     {
         pdrawenv = &g_db.draw0;
-        sceGsSetHalfOffset(&g_db.draw1, 0x800, 0x800, (odev & 0xffffU) + 1 & 1);
+
+        sceGsSetHalfOffset(&g_db.draw1, 0x800, 0x800, (odev + 1) & 1);
     }
-    
+
     sys_wrk.count++;
+
     fr++;
-    
+
     odev = sceGsSyncV(0) ^ 1;
+
     FlushCache(0);
     sceGsSwapDBuff(&g_db, fr);
     PadSyncCallback();
-    
+
     stop_put_draw_env = 0;
+
     *REG_RCNT1_COUNT = 0;
-    
+
     StartPerformanceCounter();
 }
 
@@ -268,6 +298,7 @@ void SetSysBackColor(u_char r, u_char g, u_char b)
     g_db.clear0.rgbaq.R = r;
     g_db.clear0.rgbaq.G = g;
     g_db.clear0.rgbaq.B = b;
+
     g_db.clear1.rgbaq.R = r;
     g_db.clear1.rgbaq.G = g;
     g_db.clear1.rgbaq.B = b;
