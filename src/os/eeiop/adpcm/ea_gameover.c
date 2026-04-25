102fcc309126f249f5abce8d1884bcc61bccfa66
Claudio Baecchi <claudio.baecchi@gmail.com>
2026-04-25 17:41:13 +0200

    decompiled (jp) and revisited (us,eu) os/eeiop/adpcm/ea_gameover.c

--- src/os/eeiop/adpcm/ea_gameover.c
+++ src/os/eeiop/adpcm/ea_gameover.c
@@ -7,11 +7,11 @@
 #include "os/eeiop/adpcm/ea_cmd.h"
 #include "os/eeiop/adpcm/ea_dat.h"
 
-#define DEFAULT_PITCH 0xFFF
+#define DEFAULT_PITCH 0xfff
 
 void EAdpcmGoverMain(void)
-{  
-    if (adpcm_map.mode != ADPCM_MODE_GOVER) 
+{
+    if (adpcm_map.mode != ADPCM_MODE_GOVER)
     {
         adpcm_map.mode = ADPCM_MODE_GOVER;
         adpcm_map.gover.mode = GAMEOVER_INIT;
@@ -19,29 +19,33 @@ void EAdpcmGoverMain(void)
 
     switch (adpcm_map.gover.mode)
     {
-        case GAMEOVER_INIT:
-            EAdpcmCmdStop(0, 0, 0x50);
-            adpcm_map.gover.mode = GAMEOVER_WHITE_OUT; 
-        break;
-        case GAMEOVER_WHITE_OUT:
-            if (EAGetRetStat() == 1 || EAGetRetStat() == 2) 
-            {
-                EAdpcmCmdPlay(0, 0, adpcm_map.gover.para.file_no, 0, adpcm_map.gover.para.vol, adpcm_map.gover.para.pan, adpcm_map.gover.para.pitch, adpcm_map.gover.para.fin_flm);
-                adpcm_map.gover.mode = GAMEOVER_LOAD_WAIT;
-            }
-        break;
-        case GAMEOVER_LOAD_WAIT:
-            if (EAGetRetStat() > 5) 
-            {
-                adpcm_map.gover.mode = GAMEOVER_WHITE_IN;
-            }
-        break;
-        case GAMEOVER_WHITE_IN:
-            if (EAGetRetStat() == 1 || EAGetRetStat() == 2) 
-            {
-                adpcm_map.gover.use = GAMEOVER_INIT;
-                AdpcmMapCtrlInit();
-            }
+    case GAMEOVER_INIT:
+        EAdpcmCmdStop(0, 0, 80);
+
+        adpcm_map.gover.mode = GAMEOVER_WHITE_OUT;
+    break;
+    case GAMEOVER_WHITE_OUT:
+        if (EAGetRetStat() == 1 || EAGetRetStat() == 2)
+        {
+            EAdpcmCmdPlay(0, 0, adpcm_map.gover.para.file_no, 0, adpcm_map.gover.para.vol, adpcm_map.gover.para.pan, adpcm_map.gover.para.pitch, adpcm_map.gover.para.fin_flm);
+
+            adpcm_map.gover.mode = GAMEOVER_LOAD_WAIT;
+        }
+    break;
+    case GAMEOVER_LOAD_WAIT:
+        if (EAGetRetStat() > 5)
+        {
+            adpcm_map.gover.mode = GAMEOVER_WHITE_IN;
+        }
+    break;
+    case GAMEOVER_WHITE_IN:
+        if (EAGetRetStat() == 1 || EAGetRetStat() == 2)
+        {
+            adpcm_map.gover.use = GAMEOVER_INIT;
+
+            AdpcmMapCtrlInit();
+        }
+    break;
     }
 }
 
@@ -49,7 +53,7 @@ void AdpcmPlayGameOver(int file_no, u_short vol, int fade_flm)
 {
     adpcm_map.gover.para.file_no = file_no;
     adpcm_map.gover.para.vol = GetAdpcmVol(file_no);
-    adpcm_map.gover.para.fin_flm = (u_short)fade_flm;
+    adpcm_map.gover.para.fin_flm = fade_flm;
     adpcm_map.gover.para.pan = 0x280;
     adpcm_map.gover.para.pitch = DEFAULT_PITCH;
     adpcm_map.gover.use = 1;
@@ -59,9 +63,10 @@ void AdpcmPlayGameOver(int file_no, u_short vol, int fade_flm)
 
 void AdpcmStopGameOver(int fout_flm)
 {
-    if (adpcm_map.gover.use) 
+    if (adpcm_map.gover.use)
     {
       EAdpcmCmdStop(0, 0, fout_flm);
+
       adpcm_map.gover.mode = GAMEOVER_WHITE_IN;
     }
 }
