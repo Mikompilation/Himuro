aba8c71c7270bed7d0d7f5f60ebc72ab53f5839f
Claudio Baecchi <claudio.baecchi@gmail.com>
2026-04-25 11:06:12 +0200

    decompiled (jp) and revisited (us,eu) os/eeiop/adpcm/ea_dat.c

--- src/os/eeiop/adpcm/ea_dat.c
+++ src/os/eeiop/adpcm/ea_dat.c
@@ -2,9 +2,8 @@
 #include "typedefs.h"
 #include "ea_dat.h"
 
-#include "data/adpcm_vol_tbl.h"  // ADPCM_VOL adpcm_vol_tbl[NO_VOL_TABLE] = {...};
+#include "data/adpcm_vol_tbl.h"  // ADPCM_VOL adpcm_vol_tbl[NO_VOL_TABLE];
 
-// file_no receives values from CD_FILE_DAT
 u_short GetAdpcmVol(int file_no)
 {
     ADPCM_VOL *avp;
@@ -18,8 +17,9 @@ u_short GetAdpcmVol(int file_no)
         if (avp->file_no == file_no)
         {
             vol = avp->vol;
+
             break;
-        }        
+        }
     }
 
     if (vol > MAX_VOLUME) 
@@ -29,4 +29,3 @@ u_short GetAdpcmVol(int file_no)
 
     return vol;
 }
-
