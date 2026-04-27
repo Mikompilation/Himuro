de6e06dc9d696365b8c23e733b22167526915f29
Claudio Baecchi <claudio.baecchi@gmail.com>
2026-04-27 21:15:50 +0200

    decompiled (jp) and revisited (us,eu) os/eeiop/adpcm/ea_hiso.c

--- src/os/eeiop/adpcm/ea_hiso.c
+++ src/os/eeiop/adpcm/ea_hiso.c
@@ -10,54 +10,56 @@
 void AdpcmPlayHiso(int file_no, int fade_flm)
 {
     int call_no;
-    
+
     switch(file_no)
     {
-    case 33:
+#if defined(BUILD_US_VERSION) || defined(BUILD_EU_VERSION)
+    case ADPCM_FURN_CHIKUONKI:
         call_no = AE003_CHIKUONKI_STR;
     break;
-    case 13:
+#endif
+    case ADPCM_MC_DOKONIIKUNO:
         call_no = AVMC021_DOKONIIKUNO_STR;
     break;
-    case 14:
+    case ADPCM_MC_SAMUI:
         call_no = AVMC022_SAMUI_STR;
     break;
-    case 15:
+    case ADPCM_MC_MITAWAYO:
         call_no = AVMC023_MITAWAYO_STR;
     break;
-    case 16:
+    case ADPCM_MC_KAESHITE:
         call_no = AVMC024_KAESHITE_STR;
     break;
-    case 17:
+    case ADPCM_MC_KOKONIITE:
         call_no = AVMC025_KOKONIITE_STR;
     break;
-    case 18:
+    case ADPCM_MC_NE:
         call_no = AVMC026_NE_STR;
     break;
-    case 19:
+    case ADPCM_MC_DOUSHITE:
         call_no = AVMC027_DOUSHITE_STR;
     break;
-    case 20:
+    case ADPCM_MC_NAWAGA:
         call_no = AVMC028_NAWAGA_STR;
     break;
-    case 21:
+    case ADPCM_MC_SUSURI1:
         call_no = AVMC029_SUSURI1_STR;
     break;
-    case 22:
+    case ADPCM_MC_SUSURI2:
         call_no = AVMC030_SUSURI2_STR;
     break;
-    case 23:
+    case ADPCM_MC_SUSURI3:
         call_no = AVMC031_SUSURI3_STR;
     break;
-    case 24:
+    case ADPCM_MC_SUSURI4:
         call_no = AVMC032_SUSURI4_STR;
     break;
-    case 12:
+    case ADPCM_MC_KOROSHITA:
     default:
         call_no = AVMC020_KOROSHITA_STR;
     break;
     }
-    
+
     if (adpcm_map.hiso.use != 0)
     {
         if (adpcm_map.mode == ADPCM_MODE_HISO)
@@ -82,7 +84,7 @@ void AdpcmPlayHiso(int file_no, int fade_flm)
 }
 
 void AdpcmStopHiso(int fout_flm)
-{   
+{
     if (adpcm_map.hiso.use != 0)
     {
         if (IsHighModeUse(3) != 0)
@@ -98,13 +100,10 @@ void AdpcmStopHiso(int fout_flm)
 }
 
 void EAdpcmHisoMain()
-{    
+{
     if (adpcm_map.mode != ADPCM_MODE_HISO)
     {
-        if (
-            adpcm_map.mode == ADPCM_MODE_MAP ||
-            adpcm_map.mode == ADPCM_MODE_FURN
-        )
+        if (adpcm_map.mode == ADPCM_MODE_MAP || adpcm_map.mode == ADPCM_MODE_FURN)
         {
             if (adpcm_map.hiso.mode == AMHS_MODE_END)
             {
@@ -124,18 +123,20 @@ void EAdpcmHisoMain()
             else
             {
                 EAdpcmCmdStop(0, 0, 0);
+
                 adpcm_map.hiso.count = 0;
                 adpcm_map.hiso.mode = AMHS_MODE_REQ_WAIT_STOP;
             }
         }
-        
+
         adpcm_map.mode = ADPCM_MODE_HISO;
     }
-    
+
     switch(adpcm_map.hiso.mode)
     {
     case AMHS_MODE_PRE_FADE_OUT:
-        EAdpcmCmdStop(0, 0, 0x1e);
+        EAdpcmCmdStop(0, 0, 30);
+
         adpcm_map.hiso.mode = AMHS_MODE_REQ_WAIT_STOP;
         adpcm_map.hiso.count = 0;
     break;
@@ -143,14 +144,16 @@ void EAdpcmHisoMain()
         if (EAGetRetStat() == 1 || EAGetRetStat() == 2)
         {
             adpcm_map.m_flg = 2;
+
             EAdpcmCmdPlay(0, 0, adpcm_map.hiso.para.file_no, 0, adpcm_map.hiso.para.vol, adpcm_map.hiso.para.pan, adpcm_map.hiso.para.pitch, 0);
+
             adpcm_map.hiso.mode = AMHS_MODE_REQ_PLAY;
             adpcm_map.mvol = 0xfff;
         }
         else
         {
             adpcm_map.hiso.count++;
-            
+
             if (adpcm_map.hiso.count == 200)
             {
                 EAdpcmCmdStop(0, 0, 0);
@@ -158,6 +161,7 @@ void EAdpcmHisoMain()
             else if (adpcm_map.hiso.count > 500)
             {
                 EAdpcmCmdStop(0, 0, 0);
+
                 adpcm_map.hiso.count = 0;
                 adpcm_map.hiso.use = 0;
             }
@@ -168,11 +172,12 @@ void EAdpcmHisoMain()
         {
             adpcm_map.hiso.use = 0;
         }
-        
+
         adpcm_map.mvol = 0xfff;
     break;
     case AMHS_MODE_REQ_STOP:
         EAdpcmCmdStop(0, 0, adpcm_map.hiso.para.fout_flm);
+
         adpcm_map.hiso.mode = AMHS_MODE_END;
         adpcm_map.mvol = 0xfff;
     break;
@@ -182,7 +187,7 @@ void EAdpcmHisoMain()
             adpcm_map.hiso.mode = AMHS_MODE_PRE_FADE_OUT;
             adpcm_map.hiso.use = 0;
         }
-        
+
         adpcm_map.mvol = 0xfff;
     break;
     }
