ac5b655d3a17963e984e9a813bd168f52f3c49a1
Claudio Baecchi <claudio.baecchi@gmail.com>
2026-04-28 18:43:20 +0200

    decompiled (jp) and revisited (us,eu) os/eeiop/adpcm/ea_furn.c

--- src/os/eeiop/adpcm/ea_furn.c
+++ src/os/eeiop/adpcm/ea_furn.c
@@ -3,14 +3,13 @@
 #include "enums.h"
 #include "ea_furn.h"
 
-#include "os/eeiop/adpcm/ea_dat.h"
-#include "os/eeiop/adpcm/ea_ctrl.h"
-#include "os/eeiop/adpcm/ea_cmd.h"
-
-#include "graphics/graph3d/sglib.h"
-#include "os/eeiop/se_cmd.h"
 #include "common/ul_math.h"
+#include "graphics/graph3d/sglib.h"
 #include "main/glob.h"
+#include "os/eeiop/adpcm/ea_cmd.h"
+#include "os/eeiop/adpcm/ea_ctrl.h"
+#include "os/eeiop/adpcm/ea_dat.h"
+#include "os/eeiop/se_cmd.h"
 
 static void EAdpcmFurnParaSet(ADPCM_TUNE_PARAM *atpp);
 
@@ -22,44 +21,48 @@ void EAdpcmFurnMain()
         {
             adpcm_map.furn.mode = AMFR_MODE_PRE_FADE_OUT;
         }
+        else if (adpcm_map.furn.mode == AMFR_MODE_END)
+        {
+            adpcm_map.furn.mode = AMFR_MODE_REQ_STOP;
+        }
         else
         {
-            if (adpcm_map.furn.mode == AMFR_MODE_END)
-            {
-                adpcm_map.furn.mode = AMFR_MODE_REQ_STOP;
-            }
-            else
-            {
-                adpcm_map.furn.mode = AMFR_MODE_PRE_FADE_OUT;
-            }
+            adpcm_map.furn.mode = AMFR_MODE_PRE_FADE_OUT;
         }
-        
+
         adpcm_map.mode = ADPCM_MODE_FURN;
     }
-    
+
     switch(adpcm_map.furn.mode)
     {
     case AMFR_MODE_PRE_FADE_OUT:
-        EAdpcmCmdStop(0, 0, 0x1e);
+        EAdpcmCmdStop(0, 0, 30);
+
         adpcm_map.furn.mode = AMFR_MODE_REQ_WAIT_STOP;
     break;
     case AMFR_MODE_REQ_WAIT_STOP:
         if (EAGetRetStat() == 1 || EAGetRetStat() == 2)
         {
             adpcm_map.mpara = adpcm_map.furn.para;
+
             EAdpcmFurnParaSet(&adpcm_map.mpara);
             EAdpcmCmdPlay(0, 1, adpcm_map.mpara.file_no, 0, adpcm_map.mpara.vol, adpcm_map.mpara.pan, adpcm_map.mpara.pitch, 0);
+
             adpcm_map.furn.mode = AMFR_MODE_REQ_PLAY;
         }
     break;
     case AMFR_MODE_REQ_PLAY:
         adpcm_map.mpara = adpcm_map.furn.para;
+
         EAdpcmFurnParaSet(&adpcm_map.mpara);
+
         adpcm_map.m_flg = 0;
+
         EAdpcmCmdPos(0, adpcm_map.mpara.file_no, (adpcm_map.mpara.vol * adpcm_map.mvol) / 0xfff, adpcm_map.mpara.pan, adpcm_map.mpara.pitch);
     break;
     case AMFR_MODE_REQ_STOP:
         EAdpcmCmdStop(0, 0, adpcm_map.furn.para.fout_flm);
+
         adpcm_map.furn.mode = AMFR_MODE_END;
     break;
     case AMFR_MODE_END:
@@ -78,19 +81,24 @@ static void EAdpcmFurnParaSet(ADPCM_TUNE_PARAM *atpp)
     float rot_cam;
     float rot_oc;
     float dist_obj;
-    
+
     if (atpp)
     {
-        rot_oc = SgAtan2f(camera.i[0] - camera.p[0], camera.i[2] - camera.p[2]);
-        rot_cam = SgAtan2f(atpp->pos[0] - camera.p[0], atpp->pos[2] - camera.p[2]);
+        rot_oc = VER_ATAN2F(camera.i[0] - camera.p[0], camera.i[2] - camera.p[2]);
+        rot_cam = VER_ATAN2F(atpp->pos[0] - camera.p[0], atpp->pos[2] - camera.p[2]);
+
         rot_cam = SeCmdGetAngle(rot_cam, rot_oc);
-        dist_obj = GetDistV(atpp->pos, plyr_wrk.move_box.pos);
-        dist_obj = GetDist(dist_obj, atpp->pos[1] - plyr_wrk.move_box.pos[1]);
+
+        dist_obj = GetDist(GetDistV(atpp->pos, plyr_wrk.move_box.pos), atpp->pos[1] - plyr_wrk.move_box.pos[1]);
+
         atpp->pan = EAdpcmGetPan(rot_cam);
         atpp->vol = EAdpcmGetVol(dist_obj, atpp->vol);
+
         cnt++;
+
         atpp->pitch = 0xfff;
-        if (cnt % 0x1e == 0)
+
+        if (cnt % 30 == 0)
         {
             cnt = 0;
         }
@@ -106,62 +114,68 @@ void AdpcmPlayFurn(int file_no, float *pos, u_short vol, int fade_flm)
 
     switch(file_no)
     {
-    case 0:
+    case FSPE_AE_TAPE:
         adpcm_map.furn.para.file_no = AE000_TAPE_STR;
     break;
-    case 1:
+    case FSPE_AE_KOTO:
         adpcm_map.furn.para.file_no = AE002_KOTO_STR;
     break;
-    case 25:
+#if defined(BUILD_JP_VERSION)
+    case FSPE_AE_CHIKUON:
+#elif defined(BUILD_US_VERSION) || defined(BUILD_EU_VERSION)
+    case FSPE_AE_CHIKUON:
+#endif
         adpcm_map.furn.para.file_no = AE003_CHIKUONKI_STR;
     break;
-    case 3:
+    case FSPE_AE_OKYO:
         adpcm_map.furn.para.file_no = AE004_AYASHII_STR;
     break;
-    case 4:
+    case FSPE_AE_OKYO_L:
         adpcm_map.furn.para.file_no = AK006_AME_STR;
     break;
-    case 5:
+#if defined(BUILD_US_VERSION) || defined(BUILD_EU_VERSION)
+    case FSPE_AE_DOKO:
         adpcm_map.furn.para.file_no = AVMC021_DOKONIIKUNO_STR;
     break;
-    case 6:
+    case FSPE_AE_SAMUI:
         adpcm_map.furn.para.file_no = AVMC022_SAMUI_STR;
     break;
-    case 7:
+    case FSPE_AE_MITA:
         adpcm_map.furn.para.file_no = AVMC023_MITAWAYO_STR;
     break;
-    case 8:
+    case FSPE_AE_KAESHITE:
         adpcm_map.furn.para.file_no = AVMC024_KAESHITE_STR;
     break;
-    case 9:
+    case FSPE_AE_KOKONI:
         adpcm_map.furn.para.file_no = AVMC025_KOKONIITE_STR;
     break;
-    case 10:
+    case FSPE_AE_NE:
         adpcm_map.furn.para.file_no = AVMC026_NE_STR;
     break;
-    case 11:
+    case FSPE_AE_DOUSHITE:
         adpcm_map.furn.para.file_no = AVMC027_DOUSHITE_STR;
     break;
-    case 12:
+    case FSPE_AE_NAWAGA:
         adpcm_map.furn.para.file_no = AVMC028_NAWAGA_STR;
     break;
-    case 13:
+    case FSPE_AE_SUSURI1:
         adpcm_map.furn.para.file_no = AVMC029_SUSURI1_STR;
     break;
-    case 14:
+    case FSPE_AE_SUSURI2:
         adpcm_map.furn.para.file_no = AVMC030_SUSURI2_STR;
     break;
-    case 15:
+    case FSPE_AE_SUSURI3:
         adpcm_map.furn.para.file_no = AVMC031_SUSURI3_STR;
     break;
-    case 16:
+    case FSPE_AE_SUSURI4:
         adpcm_map.furn.para.file_no = AVMC032_SUSURI4_STR;
     break;
+#endif
     default:
         adpcm_map.furn.para.file_no = AE001_USIRO_STR;
     break;
     }
-    
+
     adpcm_map.furn.para.pos[0] = pos[0];
     adpcm_map.furn.para.pos[1] = pos[1];
     adpcm_map.furn.para.pos[2] = pos[2];
@@ -170,7 +184,6 @@ void AdpcmPlayFurn(int file_no, float *pos, u_short vol, int fade_flm)
     adpcm_map.furn.para.fin_flm = fade_flm;
     adpcm_map.furn.use = 1;
 }
-
 
 void AdpcmStopFurn(int fout_flm)
 {
