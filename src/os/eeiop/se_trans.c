75bdbd976323fabfb00e6856229bc89416da9415
Claudio Baecchi <claudio.baecchi@gmail.com>
2026-04-30 16:15:27 +0200

    decompiled (jp) and revisited (us,eu) os/eeiop/se_trans.c

--- src/os/eeiop/se_trans.c
+++ src/os/eeiop/se_trans.c
@@ -1,7 +1,9 @@
 #include "common.h"
 #include "typedefs.h"
+#include "addresses.h"
 #include "enums.h"
 #include "se_trans.h"
+
 #include "sce/sif.h"
 
 #include "main/glob.h"
@@ -11,7 +13,7 @@
 #include "ingame/entry/entry.h"
 #include "ingame/enemy/ene_ctl.h"
 
-#include "data/fgf_tbl.h" // FG_FILE_TBL fgf_tbl[/*4*/] = { ... };
+#include "data/fgf_tbl.h" // FG_FILE_TBL fgf_tbl[];
 SE_TRANS_CTRL st_ctrl = {0};
 
 static u_char ChkLoadReqQueueFree();
@@ -27,24 +29,26 @@ static void UseOkFGhost();
 #define MAX_TRANS_SIZE 0x64000
 
 void FGTransInit()
-{  
-    if (ingame_wrk.msn_no != PMODE_NORMAL && ingame_wrk.msn_no != PMODE_MSG_DISP) 
+{
+    if (ingame_wrk.msn_no != PMODE_NORMAL && ingame_wrk.msn_no != PMODE_MSG_DISP)
     {
-        if (ChkFGhostAlreadySet()) 
+        if (ChkFGhostAlreadySet())
         {
             st_ctrl.mode = STMODE_USEOK;
-            ap_wrk.stts = ap_wrk.stts & 0xbf;
+            ap_wrk.stts &= ~0x40;
+
             return;
         }
-        if (ChkLoadReqQueueFree() && ChkNeedsFGhostSe()) 
+
+        if (ChkLoadReqQueueFree() && ChkNeedsFGhostSe())
         {
             st_ctrl.mode = STMODE_READY;
+
             return;
         }
     }
-    
+
     st_ctrl.mode = STMODE_USEOK;
-    return;
 }
 
 u_char IsEndFgTrans()
@@ -78,19 +82,21 @@ static u_char ChkNeedsFGhostSe()
 {
     int i;
     u_char ret;
-  
+
     ret = 0;
 
-    for (i = 0; i < 3; i++) 
+    for (i = 0; i < 3; i++)
     {
-        if (ap_wrk.fg_se_empty[i] == 0) 
+        if (ap_wrk.fg_se_empty[i] == 0)
         {
             st_ctrl.trans_pos = i;
+
             ret = 1;
+
             break;
         }
     }
-    
+
     return ret;
 }
 
@@ -105,15 +111,15 @@ void FloatGhostSENotEmpty()
             return;
         }
     }
-  
-    ap_wrk.stts = ap_wrk.stts | 0x40;
+
+    ap_wrk.stts |= 0x40;
 }
 
 static void GetSeTransSize()
 {
     st_ctrl.size = GetImgArrangementP(st_ctrl.file_no)->size;
-    
-    if (MAX_TRANS_SIZE < st_ctrl.size) 
+
+    if (st_ctrl.size > MAX_TRANS_SIZE)
     {
         st_ctrl.size = MAX_TRANS_SIZE;
     }
@@ -122,8 +128,8 @@ static void GetSeTransSize()
 static void DmaTransReq()
 {
     sceSifDmaData dma;
-  
-    dma.data = 0x1460000;
+
+    dma.data = LOAD_ADDRESS_22;
     dma.addr = rcv_stat.cdvd.ld_addr;
     dma.size = st_ctrl.size;
     dma.mode = 0;
@@ -155,40 +161,49 @@ static void UseOkFGhost()
 {
     se_ctrl.ghost_no[st_ctrl.trans_pos] = st_ctrl.file_no;
     se_ctrl.ghost_type[st_ctrl.trans_pos] = 1;
-    ap_wrk.stts = ap_wrk.stts & 0xbf;
+
+    ap_wrk.stts &= ~0x40;
 }
 
 int SeFGhostTransCtrl()
 {
-    int ret = 0;
+    int ret;
+
+    ret = 0;
+
     sys_wrk.sreset_count = 0;
-    
+
     switch (st_ctrl.mode)
     {
-        case STMODE_READY:
-            GetSeTransSize();
-            DmaTransReq();
-            st_ctrl.mode = STMODE_EE2IOP;
-        case STMODE_EE2IOP:
-            if (ChkDmaTransEndSe()) 
-            {
-                TransReqIopCmd();
-                st_ctrl.mode = STMODE_IOP2SPU;
-            }
-        break;
-        case STMODE_IOP2SPU:
-            // Line 386
-            if (!TransWaitIopCmd()) 
-            {
-                break;
-            }
-                
-            SetIopCmdSm(IC_CDVD_SE_TRANS_RESET, 0, 0, 0);
-            UseOkFGhost();
-            st_ctrl.mode = STMODE_USEOK;
+    case STMODE_READY:
+        GetSeTransSize();
+        DmaTransReq();
+
+        st_ctrl.mode = STMODE_EE2IOP;
+    // case fall-through
+    case STMODE_EE2IOP:
+        if (ChkDmaTransEndSe())
+        {
+            TransReqIopCmd();
+
+            st_ctrl.mode = STMODE_IOP2SPU;
+        }
+    break;
+    case STMODE_IOP2SPU:
+        if (TransWaitIopCmd() == 0)
+        {
             break;
-        case STMODE_USEOK: ret = 1;
+        }
+
+        SetIopCmdSm(IC_CDVD_SE_TRANS_RESET, 0, 0, 0);
+        UseOkFGhost();
+
+        st_ctrl.mode = STMODE_USEOK;
+    break;
+    case STMODE_USEOK:
+        ret = 1;
+    break;
     }
-    
+
     return ret;
 }
