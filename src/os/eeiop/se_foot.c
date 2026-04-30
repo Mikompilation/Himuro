4555163642b91a0a93a22a7e7789e816e9a144ec
Claudio Baecchi <claudio.baecchi@gmail.com>
2026-04-30 16:33:23 +0200

    decompiled (jp) and revisited (us,eu) os/eeiop/se_foot.c

--- src/os/eeiop/se_foot.c
+++ src/os/eeiop/se_foot.c
@@ -3,11 +3,11 @@
 #include "enums.h"
 #include "se_foot.h"
 
-#include "os/eeiop/eese.h"
-#include "os/eeiop/sd_room.h"
 #include "ingame/map/door_ctl.h"
 // #include "ingame/map/map_ctrl.h"
 #include "main/glob.h"
+#include "os/eeiop/eese.h"
+#include "os/eeiop/sd_room.h"
 
 static void SFootNoUse(u_char room_id0, u_char room_id1);
 static void SFootSetLoadTbl(u_int *ld_tbl, u_char room_id);
@@ -17,22 +17,20 @@ static void SFootNoUse(u_char room_id0, u_char room_id1)
     u_int *fp;
     u_int *sedp;
     int i;
-    int j; // looks like gcc optimized this out by using the stack address (sp) 
+    int j;
     int k;
     u_char room_id[2];
     u_char exist_flg;
-    
-    sedp = se_ctrl.foot_no;
-    
+
     room_id[0] = room_id0;
     room_id[1] = room_id1;
-    
-    for (i = 0; i < 8; i++, sedp++)
+
+    for (i = 0, sedp = se_ctrl.foot_no; i < 8; i++, sedp++)
     {
         if (*sedp != 0xffffffff)
         {
             exist_flg = 0;
-            
+
             for (j = 0; j < 2; j++)
             {
                 if (room_id[j] != 0xff)
@@ -42,13 +40,14 @@ static void SFootNoUse(u_char room_id0, u_char room_id1)
                         if (*sedp == *fp)
                         {
                             exist_flg = 1;
-                            goto found;
+
+                            goto found; // needed to break both loops
                         }
                     }
                 }
             }
-            
-            found:
+
+found:
             if (!exist_flg)
             {
                 *sedp = 0xffffffff;
@@ -72,23 +71,23 @@ static void SFootSetLoadTbl(u_int *ld_tbl, u_char room_id)
     for (i = 0, ifp = GetSdrFootSeTblP(room_id); i < 5; i++, ifp++)
     {
         vflg = 0;
-        
+
         if (*ifp != 0xffffffff)
         {
             for (j = 0; j < 8; j++)
             {
-                
+
                 if (se_ctrl.foot_no[j] == *ifp)
                 {
                     vflg = 1;
+
                     break;
                 }
             }
-            
+
             if (!vflg)
             {
-                *ld_tbl = *ifp;
-                ld_tbl++;
+                *ld_tbl++ = *ifp;
             }
         }
     }
@@ -100,10 +99,10 @@ int SFootLoadReqAndSetSub(int load_id, u_char room_from, u_char room_to)
     u_char empty_tbl[8];
     int i;
     int j;
-    
+
     SFootNoUse(room_from, room_to);
     SFootSetLoadTbl(load_file_tbl, room_to);
-    
+
     for (i = 0; i < 8; i++)
     {
         if (se_ctrl.foot_no[i] != 0xffffffff)
@@ -115,7 +114,7 @@ int SFootLoadReqAndSetSub(int load_id, u_char room_from, u_char room_to)
             empty_tbl[i] = 0;
         }
     }
-    
+
     for (i = 0; i < 5; i++)
     {
         if (load_file_tbl[i] != -1)
@@ -125,7 +124,9 @@ int SFootLoadReqAndSetSub(int load_id, u_char room_from, u_char room_to)
                 if (empty_tbl[j] == 0)
                 {
                     load_id = SeFileLoadAndSet(load_file_tbl[i], j + 6);
+
                     empty_tbl[j] = 1;
+
                     break;
                 }
             }
@@ -142,20 +143,25 @@ int SFootLoadReqAndSet(int load_id, u_short door_id)
     u_char room_from;
     u_char room_to;
     u_char dr_num;
-    
+
     room_from = GetRoomIdFromRoomNo(0, room_wrk.room_no);
     addr_uc = GetDoorRoomConectDataP(room_from);
 
     dr_num = addr_uc[0];
     room_to = 0xff;
-    
-    for (i = 0, addr_uc = &addr_uc[2]; i < dr_num; i++, addr_uc = &addr_uc[4])
+
+    addr_uc = &addr_uc[2];
+
+    for (i = 0; i < dr_num; i++)
     {
         if (*(u_short *)addr_uc == door_id)
         {
             room_to = addr_uc[2];
+
             break;
         }
+
+        addr_uc = &addr_uc[4];
     }
 
     if (room_to != 0xff)
@@ -178,20 +184,20 @@ u_char SFootGetSeNo(int call_no)
     int i;
     int file_no;
 
-    
-    if (call_no >= 0 && call_no < 0x1c)
+
+    if (call_no >= 0 && call_no < 28)
     {
         file_no = foot_se_index[call_no];
-        
+
         for (i = 0; i < 8; i++)
         {
             if (se_ctrl.foot_no[i] == file_no)
             {
-                return i + 0x2c;
+                return i + 44;
             }
         }
     }
-    
+
     return 0xff;
 }
 
@@ -199,16 +205,13 @@ u_char SFootGetSeNoFromFileNo(int file_no)
 {
     int i;
 
-    int iVar1;
-    u_int *puVar2;
-    
-    for (i = 0; i < 8; i++) 
+    for (i = 0; i < 8; i++)
     {
         if (se_ctrl.foot_no[i] == file_no && file_no != -1)
         {
-            return i + 0x2c;
+            return i + 44;
         }
     }
-    
+
     return 0xff;
 }
