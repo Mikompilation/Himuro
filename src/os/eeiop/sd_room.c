b36693383c51513035b5daf87a822e7bfa643ba7
Claudio Baecchi <claudio.baecchi@gmail.com>
2026-04-30 16:47:35 +0200

    decompiled (jp) and revisited (us,eu) os/eeiop/sd_room.c

--- src/os/eeiop/sd_room.c
+++ src/os/eeiop/sd_room.c
@@ -16,17 +16,17 @@ typedef struct {
   u_int fno[5];
 } ROOM_FOOT_SE;
 
-#include "data/adpcm_param_tbl.h" // ADPCM_ROOM_PLAY adpcm_param_tbl[] = { ... };
-#include "data/foot_se_index.h" // int foot_se_index[] = { ... };
-#include "data/rm_snd_info.h" // static ROOM_SOUND_INFO rm_snd_info[] = { ... };
-#include "data/rm_door.h" // static ROOM_DOOR_SE rm_door[] = { ... };
-#include "data/rm_foot.h" // static ROOM_FOOT_SE rm_foot[] = { ... };
-#include "data/rm_srund.h" // static u_int rm_srund[] = { ... };
-#include "data/rm_event.h" // static u_int rm_event[] = { ... };
+#include "data/adpcm_param_tbl.h" // ADPCM_ROOM_PLAY adpcm_param_tbl[];
+#include "data/foot_se_index.h" // int foot_se_index[];
+#include "data/rm_snd_info.h" // static ROOM_SOUND_INFO rm_snd_info[];
+#include "data/rm_door.h" // static ROOM_DOOR_SE rm_door[];
+#include "data/rm_foot.h" // static ROOM_FOOT_SE rm_foot[];
+#include "data/rm_srund.h" // static u_int rm_srund[];
+#include "data/rm_event.h" // static u_int rm_event[];
 
 ROOM_SOUND_INFO* GetSdrDatP(u_char room_id)
 {
-  return &rm_snd_info[room_id];
+    return &rm_snd_info[room_id];
 }
 
 int GetSdrAdpcmFno(u_char room_id)
@@ -47,31 +47,31 @@ u_short GetSdrReverbVol(u_char room_id)
 void SetRoomReverbVol(u_char room_id)
 {
     u_short vol;
-  
-    if (room_id >= RM_DOOR_SIZE) 
+
+    if (room_id >= RM_DOOR_SIZE)
     {
         vol = MAX_VOL;
-        
+
     }
-    else 
+    else
     {
         vol = GetSdrReverbVol(room_id);
     }
-    
+
     SetReverbVolume(vol);
 }
 
 u_int * GetSdrDoorSeTblP(u_char room_id)
 {
     u_char door_no;
-  
+
     door_no = rm_snd_info[room_id].door_no;
 
     if (ingame_wrk.msn_no != 1 && room_id == 13)
     {
         door_no = RM_DOOR_SIZE;
     }
-    
+
     return rm_door[door_no].fno;
 }
 
