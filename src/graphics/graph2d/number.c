8f8a3e6340f1445c8a4d6506585bac76d992ea33
Claudio Baecchi <claudio.baecchi@gmail.com>
2026-06-30 17:29:49 +0200

    decompiled (jp) and revisited (us,eu) graphics/graph2d/number.c

--- src/graphics/graph2d/number.c
+++ src/graphics/graph2d/number.c
@@ -2,21 +2,20 @@
 #include "typedefs.h"
 #include "number.h"
 
-#include "outgame/mode_slct.h"
+// #include "graphics/graph2d/message.h"
 #include "graphics/graph2d/sprt.h"
-
-// #include "graphics/graph2d/message.h" // DO NOT IMPORT!!
-// #include "graphics/graph2d/tim2.h" // DO NOT IMPORT!!
+// #include "graphics/graph2d/tim2.h"
+#include "outgame/mode_slct.h"
 
 #include "data/number_tex.h" // SPRT_DAT number_tex[][10];
 
 void NumberDisp(int number, short int pos_x, short int pos_y, u_char font, u_char r, u_char g, u_char b, u_char alpha, int pri, int digit, int type)
 {
-	int i;
-	int multi10;
-	int multi10_bak;
-	int disp_number;
-	int disp_digit;
+    int i;
+    int multi10;
+    int multi10_bak;
+    int disp_number;
+    int disp_digit;
 
     multi10 = 1;
 
@@ -58,6 +57,7 @@ void NumberDisp(int number, short int pos_x, short int pos_y, u_char font, u_cha
             if (digit - disp_digit <= i)
             {
                 NumberFontDisp(disp_number / multi10, i, pos_x, pos_y, font, r, g, b, pri, alpha);
+
                 disp_number -= (disp_number / multi10) * multi10;
             }
         break;
@@ -65,11 +65,13 @@ void NumberDisp(int number, short int pos_x, short int pos_y, u_char font, u_cha
             if (digit - disp_digit <= i)
             {
                 NumberFontDisp(disp_number / multi10, i - (digit - disp_digit), pos_x, pos_y, font, r, g, b, pri, alpha);
+
                 disp_number -= (disp_number / multi10) * multi10;
             }
         break;
         case 2:
             NumberFontDisp(disp_number / multi10, i, pos_x, pos_y, font, r, g, b, pri, alpha);
+
             disp_number -= (disp_number / multi10) * multi10;
         break;
         }
@@ -78,8 +80,8 @@ void NumberDisp(int number, short int pos_x, short int pos_y, u_char font, u_cha
 
 void NumberFontDisp(u_char number, u_char no, short int pos_x, short int pos_y, u_char font, u_char r, u_char g, u_char b, int pri, u_char alpha)
 {
-	u_short font_w[5] = { 0x18, 0x15, 0x18, 0x18, 0x18 };
-	DISP_SPRT ds;
+    u_short font_w[5] = { 0x18, 0x15, 0x18, 0x18, 0x18 };
+    DISP_SPRT ds;
 
     if (font == 0 || font == 3)
     {
