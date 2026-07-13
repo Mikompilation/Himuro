535465ade64a839e9dc51d5401ba79f78e42a54a
Claudio Baecchi <claudio.baecchi@gmail.com>
2026-07-13 21:57:18 +0200

    decompiled (jp) and revisited (us,eu) graphics/motion/mim_dat.c

--- src/graphics/motion/mim_dat.c
+++ src/graphics/motion/mim_dat.c
@@ -2,12 +2,12 @@
 #include "typedefs.h"
 #include "mim_dat.h"
 
-u_char mimchodo_r003[] = {0x0f, 0xff};
-u_char mimchodo_r007[] = {0x97, 0xff};
-u_char mimchodo_r009[] = {0x90, 0xff};
-u_char mimchodo_r011[] = {0x0d, 0x0e, 0x8e, 0xff};
-u_char mimchodo_r021[] = {0x91, 0xff};
-u_char mimchodo_r041[] = {0x24, 0xff};
+u_char mimchodo_r003[] = { 0x0f, 0xff };
+u_char mimchodo_r007[] = { 0x97, 0xff };
+u_char mimchodo_r009[] = { 0x90, 0xff };
+u_char mimchodo_r011[] = { 0x0d, 0x0e, 0x8e, 0xff };
+u_char mimchodo_r021[] = { 0x91, 0xff };
+u_char mimchodo_r041[] = { 0x24, 0xff };
 
 MIME_LOAD mimchodo_tbl[] = {
     { .addr = NULL, .dat = NULL, },
