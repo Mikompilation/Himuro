34004c7a4f5ac3145c57d486063d6b5ffd0951dd
Claudio Baecchi <claudio.baecchi@gmail.com>
2026-07-13 22:08:38 +0200

    decompiled (jp) and revisited (us,eu) graphics/graph3d/object.c

--- src/graphics/graph3d/object.c
+++ src/graphics/graph3d/object.c
@@ -9,20 +9,21 @@ qword *pstart3 = NULL;
 
 void objInit()
 {
-	static int bufid = 0;
-	static qword p3_buf[2][256];
-	qword *base3;
+    static int bufid = 0;
+    static qword p3_buf[2][256];
+    qword *base3;
 
     pstart3 = p3_buf[bufid];
     bufid = 1 - bufid;
     pbase3 = pstart3;
+
     SgSetWorkBase(pbase3);
 }
 
 qword *pbase1 = NULL;
 qword *pbase3 = NULL;
 
- void objFlush()
+void objFlush()
 {
     return;
 }
